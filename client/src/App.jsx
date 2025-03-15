ff2a6935326bcaeb37c0b9c1324c1ea39ac1a529
su469843 <128950064+su469843@users.noreply.github.com>
2025-03-15 10:42:12 +0800

    44

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      <div className="container">
+        {/* Todo: 添加任务列表和表单组件 */}
+      </div>
+    </div>
+  )
+}
+
+export default App
